8e76c0e17a905e2c840634eceff19784791c7ba8
yosef-girma <37630081+yosef-girma@users.noreply.github.com>
2023-06-09 04:56:29 +0300

    Update ajax_handler.js

--- xpr/server_js/ajax_handler.js
+++ xpr/server_js/ajax_handler.js
@@ -1,16 +1,7 @@
-var xpr_objects = require("/xpr/request");
-var analytics = require("XprObjects/XprAnalytics");
+
 
 exports.process = function(context, options) {
 
 
-    return "ajaxHandler";
-}
-function process(){
-    let request = xpr_objects.XprRequest();
-    var api = xpr_objects.XprApi;
-    
-
-	//analytics.trackSearch(request.urlParams.q, 'test','test');	
-    return 1;
+    return "ajaxHandler from bundle test one , updating the content";
 }
