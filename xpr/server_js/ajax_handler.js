44cede41e588c82c3296e8915f63ff7e343d84aa
yosef-girma <37630081+yosef-girma@users.noreply.github.com>
2023-07-14 01:27:07 +0300

    Update ajax_handler.js

--- xpr/server_js/ajax_handler.js
+++ xpr/server_js/ajax_handler.js
@@ -1,7 +1,6 @@
-
-
+require('/xpr/utilities');
 exports.process = function(context, options) {
-
+     PHP.XprConsole("Test logging from bundle test");
      XprConsole.log("Test logging");
-    return "ajaxHandler from bundle test one , updating the content";
+    return "ajaxHandler from bundle test one, updating the content";
 }
